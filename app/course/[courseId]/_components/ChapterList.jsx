6757dd608e0496fbc8c9abaaa68790e9519a86bc
kamlesh-Sahani <alex20150001969@gmail.com>
2025-05-11 15:26:45 +0530

    chnages

--- app/course/[courseId]/_components/ChapterList.jsx
+++ app/course/[courseId]/_components/ChapterList.jsx
@@ -29,7 +29,7 @@ function ChapterList({ chapters }) {
         <AnimatePresence>
           {chapterData && chapterData.map((chapter, index) => (
             <motion.div
-              key={chapter.id}
+              key={index}
               initial={{ opacity: 0, y: 10 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.3, delay: index * 0.05 }}
